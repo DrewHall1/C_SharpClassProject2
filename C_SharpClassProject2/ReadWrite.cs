01f77a82f2d53a20992488b574a813ab92e4feb3
William A Hall - "Drew <drew.hall.usa@outlook.com>
2019-07-26 11:33:22 -0400

    Update ReadWrite.cs
    added comment lines to the code

--- C_SharpClassProject2/ReadWrite.cs
+++ C_SharpClassProject2/ReadWrite.cs
@@ -12,6 +12,7 @@ namespace C_SharpClassProject2
 {
     public static class ReadWrite
     {
+        //This method takes my CSV and uses a stream reader to read it into a list of exercises objects. This happens right when the app opens.
         public static List<Workouts> ReadData(string fileName, List<Workouts>Exercises)
         {
             using (var reader = new StreamReader(fileName))
@@ -31,7 +32,7 @@ namespace C_SharpClassProject2
             }
             return Exercises;
         }
-
+        //This method writes all the exercises currently in the database to the console for the user to choose from.
         public static List<Workouts> ViewData(List<Workouts> Exercises)
         {
             foreach (Workouts workouts in Exercises)
@@ -40,7 +41,7 @@ namespace C_SharpClassProject2
             }
             return Exercises;
         }
-
+        //This method promts the user to add a new/different exercise to the existing list and saves it.
         public static List<Workouts> AddWorkouts(List<Workouts> Exercises)
         {
             var newWorkouts = new Workouts();
@@ -50,7 +51,7 @@ namespace C_SharpClassProject2
             Exercises.Add(newWorkouts);
             return Exercises;
         }
-
+        //This method lets the user go to an existing exercise, edit it, and save it back to the list.
         public static void EditWorkouts(List<Workouts> Exercises, string path)
         {
             Console.WriteLine("Which workout/exercise would you like to edit?");
@@ -72,7 +73,7 @@ namespace C_SharpClassProject2
             Console.WriteLine(tmp);
             EditWorkouts(tmp);
         }
-
+        //This method helps keep my code dry by holding the promts for add and edit in one place.
         public static void EditWorkouts(Workouts newWorkouts)
         {
             Console.WriteLine("Enter type of exercise: (Upper Body, Lower Body, Core Body) ");
@@ -81,7 +82,7 @@ namespace C_SharpClassProject2
             Console.WriteLine("Enter the name of the exercise: ");
             newWorkouts.Name = Console.ReadLine();
         }
-
+        //This method runs at the end and writes the list back to a string to be saved to the CSV to be used next time the app is launched.
         public static void WriteData(string fileName, List<Workouts> Exercises)
         {
             string WorkoutsString = string.Empty;
